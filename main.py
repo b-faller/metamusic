c13ed7f19d635cc723542990e664567c38011b75
Benjamin Faller <git@pwned.info>
2022-12-12 06:27:19 +0100

    Show album metadata and name cover image

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
+import json
 import logging
-from bs4 import BeautifulSoup
-import requests
 import re
+from dataclasses import dataclass, field
+
+import requests
+from bs4 import BeautifulSoup
 from PIL import Image
 
 logger = logging.getLogger(__name__)
 
 LOG_FORMAT = "[%(asctime)s] %(levelname)s: %(message)s"
+
+
+@dataclass
+class TrackInfo:
+    track_number: int
+    track_name: str
+
+    def __str__(self):
+        return f"{self.track_number}\t{self.track_name}"
+
+
+@dataclass
+class AlbumInfo:
+    artist: str
+    album: str
+    date_published: str
+    genre: str
+    tracks: list[TrackInfo] = field(default_factory=list)
+
+    @property
+    def number_of_tracks(self) -> int:
+        return len(self.tracks)
+
+    def __str__(self):
+        album_str = f"Artist:\t {self.artist}\n"
+        album_str += f"Album:\t {self.album}\n"
+        album_str += f"Date published:\t {self.date_published}\n"
+        album_str += f"Genre:\t {self.genre}\n"
+        album_str += f"Number of tracks:\t {self.number_of_tracks}\n\n"
+
+        # Print the information for each track
+        album_str += "Tracks:"
+        for track_info in self.tracks:
+            # Print the track number and name
+            album_str += "\n" + str(track_info)
+
+        return album_str
 
 
 def download_jpeg_image(url, path: str) -> None:
@@ -36,17 +76,21 @@ def get_high_resolution_album_cover_url(url):
     return new_url
 
 
-def download_cover_art(url: str, path: str) -> None:
-    # Send HTTP request to the URL
+def get_html_soup(url: str):
+   # Send HTTP request to the URL
     response = requests.get(url)
 
     # If the request is not successful (response code is not 200), return early
     if response.status_code != 200:
-        logger.error("Failed to extract the image URL from the provided URL: %s", url)
+        logger.error(
+            "Failed to extract the image URL from the provided URL: %s", url)
         return
 
     # Parse the response HTML using BeautifulSoup
-    soup = BeautifulSoup(response.text, 'html.parser')
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def download_cover_art(soup, path: str) -> None:
     # Find the div element with the "artwork-component" class
     div = soup.find('div', class_='artwork-component')
     # Find the picture element within the div
@@ -63,7 +107,7 @@ def download_cover_art(url: str, path: str) -> None:
     hq_image_url = get_high_resolution_album_cover_url(image_url)
 
     # Actually download and save the image to the filesystem
-    logging.info("Downloading image from URL: %s", hq_image_url)
+    logging.info("Downloading cover image from URL: %s", hq_image_url)
     download_jpeg_image(hq_image_url, path)
 
 
@@ -82,6 +126,43 @@ def format_cover_size(number):
     return str(number)
 
 
+def to_snake_case(s: str) -> str:
+    sb = ""
+    for c in s.strip():
+        if c.isupper() and c.isascii():
+            sb += c.lower()
+        elif c.isascii() and (c.islower() or c.isdigit()):
+            sb += c
+        elif len(sb) > 0 and sb[-1] != '_':
+            sb += '_'
+    sb = sb.rstrip("_")
+    return sb
+
+
+def parse_album_info(soup):
+    # Find the metadata script
+    json_str = soup.find("script", {"id": "schema:music-album"}).contents[0]
+
+    # Parse the JSON-LD string into a dictionary
+    data = json.loads(json_str)
+
+    # Create a list of TrackInfo objects from the parsed data
+    track_infos = [
+        TrackInfo(track_number=i+1, track_name=track["name"])
+        for i, track in enumerate(data["tracks"])
+    ]
+    # Create an AlbumInfo object from the parsed data
+    album_info = AlbumInfo(
+        artist=data["byArtist"]["name"],
+        album=data["name"],
+        date_published=data["datePublished"],
+        genre=data["genre"][0],
+        tracks=track_infos
+    )
+
+    return album_info
+
+
 def square_cover_image(path):
     with Image.open(path) as img:
         width, height = img.size
@@ -90,7 +171,7 @@ def square_cover_image(path):
         side_size = max(width, height)
 
         # Print the dimensions of the downloaded image in the format {width}x{height}
-        logger.info("Image dimensions: %sx%s", width, height)
+        logger.info("Cover image dimensions: %sx%s", width, height)
         logger.info("Cover size: %s", format_cover_size(side_size))
 
         # Check if we actually have to resize
@@ -120,12 +201,27 @@ def init_logging():
 
 def main():
     init_logging()
+
     # Prompt user for an URL
     url = input("Enter the URL of the album cover art: ")
+
+    # Get the HTML from the URL
+    soup = get_html_soup(url)
+
+    # Print the album metadata
+    album_info = parse_album_info(soup)
+    logger.info("Extracted Album metadata is:\n%s", album_info)
+
+    # Get the cover file name
+    artist_snake_case = to_snake_case(album_info.artist)
+    album_snake_case = to_snake_case(album_info.album)
+    cover_path = f"album_cover_{artist_snake_case}_{album_snake_case}.jpeg"
+
     # Call download_cover_art with the entered URL
-    download_cover_art(url, "cover.jpeg")
+    download_cover_art(soup, cover_path)
+
     # Resize the cover to be square
-    square_cover_image("cover.jpeg")
+    square_cover_image(cover_path)
 
 
 if __name__ == "__main__":
